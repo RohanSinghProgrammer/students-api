ca7fe92c2075310959dd1c41de25ce4ae47b9fbf
Rohan Singh <ff3870422@gmail.com>
2025-08-01 21:56:53 +0530

    Setup Config

--- internals/config/config.go
+++ internals/config/config.go
@@ -1,11 +1,50 @@
 package config
 
+import (
+	"flag"
+	"log"
+	"os"
+
+	"github.com/ilyakaznacheev/cleanenv"
+)
+
 type HTTPServer struct {
-	Address string `yaml:"address"`
+	Address string `yaml:"address" default:"4000"`
 }
 
 type Config struct {
-	Env		  string `yaml:"env"`
-	StoragePath string `yaml:"storage_path"`
+	Env		  string `yaml:"env"  env-require:"true"`
+	StoragePath string `yaml:"storage_path" env-require:"true"`
 	HTTPServer `yaml:"http_server"`
+}
+
+func MustLoadConfig() *Config {
+	var configPath string
+
+	configPath = os.Getenv("CONFIG_PATH")
+
+	if configPath == "" {
+		flags := flag.String("config", "", "Path to the configuration file")
+		flag.Parse()
+
+		configPath = *flags
+
+		if configPath == "" {
+			log.Fatal("CONFIG_PATH environment variable or --config flag must be set")
+		}
+	}
+
+	if _, err := os.Stat(configPath); os.IsNotExist(err) {
+		log.Fatalf("Configuration file does not exist at path: %s", configPath)
+	}
+
+	var config Config
+
+	err := cleanenv.ReadConfig(configPath, &config)
+
+	if err != nil {
+		log.Fatalf("Failed to read configuration file: %s", err.Error())
+	}
+
+	return &config
 }
