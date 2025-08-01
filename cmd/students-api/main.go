37760f88bdc44b8d5634b012a257e04359c92d2f
Rohan Singh <ff3870422@gmail.com>
2025-08-01 22:11:49 +0530

    Created Basic Server

--- cmd/students-api/main.go
+++ cmd/students-api/main.go
@@ -1,7 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+
+	"github.com/rohansinghprogrammer/sudents-api/internals/config"
+)
 
 func main()  {
-	fmt.Println("Hello, Students API!")
+	// Load Confg
+	cfg := config.MustLoadConfig()
+	// Setup DB
+
+	// Setup Routes
+	router := http.NewServeMux()
+
+	router.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello, World!"))
+	})
+	
+	// Listen Server
+	server := http.Server{
+		Addr:    cfg.Address,
+		Handler: router,
+	}
+
+	log.Printf("Starting server on %s", cfg.Address)
+	// Start Server
+	err := server.ListenAndServe()
+	if err != nil {
+		log.Fatalf("Failed to start server: %s", err.Error())
+	}
 }
